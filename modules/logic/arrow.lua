88a00b2bae56d3d38a29645fecbe9313613246df
Onran0 <kotshel2009@gmail.com>
2024-08-10 03:19:39 +0900

    заготовки для стрелочек

---
+++ modules/logic/arrow.lua
@@ -0,0 +1,15 @@
+local arrow = { }
+
+function arrow.tick(x, y, z)
+
+end
+
+function arrow.placed(x, y, z)
+
+end
+
+function arrow.broken(x, y, z)
+
+end
+
+return arrow
---
+++ scripts/wire.lua
@@ -0,0 +1,15 @@
+local blocks_tick = require 'bitwise:blocks_tick'
+local arrow = require 'bitwise:logic/arrow'
+
+blocks_tick.reg_func("bitwise:wire_off", function (x, y, z)
+    block.set(x, y+1, z, 1, 1)
+end)
+
+on_broken = arrow.broken
+
+on_placed = arrow.placed
+
+on_tick = arrow.tick
+
+blocks_tick.register("bitwise:wire_off")
+blocks_tick.register("bitwise:wire_on")
