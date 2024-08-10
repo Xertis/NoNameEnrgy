29003eec1eddcc08986cb5792e1267c9cec46aed
Onran0 <kotshel2009@gmail.com>
2024-08-10 19:34:06 +0900

    убрал вызов удалённой функции

--- scripts/wire.lua
+++ scripts/wire.lua
@@ -1,10 +1,6 @@
 local blocks_tick = require 'bitwise:blocks_tick'
 local arrow = require 'bitwise:logic/arrow'
 
-blocks_tick.reg_func("bitwise:wire_off", function (x, y, z)
-    block.set(x, y+1, z, 1, 1)
-end)
-
 on_broken = arrow.broken
 
 on_placed = arrow.placed
